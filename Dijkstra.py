4be11ad0929c21c198eb7719a2291934b3245a1c
Otavio Joshua <otaviomenezes574@gmail.com>
2025-08-23 16:49:28 -0300

    first commit

---
+++ Dijkstra.py
@@ -0,0 +1,9 @@
+
+class Grafo:
+    def __init__(self, num_vertices, num_arestas):
+        self.num_vertices = num_vertices
+        self.num_arestas = num_arestas
+        
+    def dijkstra(self):
+        pass
+
