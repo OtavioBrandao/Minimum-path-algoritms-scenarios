b85efb6cc0b69de482c232d64300a8c397e72d0f
Otavio Joshua <otaviomenezes574@gmail.com>
2025-08-23 17:07:41 -0300

    coisas iniciais

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,9 +1,7 @@
 
-class Grafo:
-    def __init__(self, num_vertices, num_arestas):
-        self.num_vertices = num_vertices
-        self.num_arestas = num_arestas
-        
-    def dijkstra(self):
-        pass
+from Graph import GraphBase
+
+
+class Grafo(GraphBase):
+    pass
 
